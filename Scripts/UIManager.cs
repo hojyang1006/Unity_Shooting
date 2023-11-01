13b01341d41a04bf4a47213a6f329dcb342cf76a
hojyang1006 <127534964+hojyang1006@users.noreply.github.com>
2023-11-01 18:36:51 +0900

    Unity_슈팅게임 소스코드

---
+++ Scripts/UIManager.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    [SerializeField]
+    private Text _scoreText;
+    [SerializeField]
+    private Image _LivesImg;
+    [SerializeField]
+    private Sprite[] _liveSprites;
+    [SerializeField]
+    private Text _gameOverText;
+    [SerializeField]
+    private Text _restartText;
+
+    private GameManager _gameManager;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+        _scoreText.text = "점수 : " + 0;
+        _gameOverText.gameObject.SetActive(false);
+        _gameManager = GameObject.Find("Game_Manager").GetComponent<GameManager>();
+
+        if (_gameManager == null)
+        {
+            Debug.LogError("게임 매니저가 비었음");
+        }
+    }
+
+    public void UpdateScore(int playerScore)
+    {
+        _scoreText.text = "점수: " + playerScore.ToString();
+    }
+
+    public void UpdateLives(int currentLives)
+    {
+        _LivesImg.sprite = _liveSprites[currentLives];
+
+        if (currentLives == 0)
+        {
+            GameOverSepuence();
+        }
+    }
+
+    void GameOverSepuence()
+    {
+        _gameManager.GameOver();
+        _gameOverText.gameObject.SetActive(true);
+        _restartText.gameObject.SetActive(true);
+        StartCoroutine(GameOverFlickerRoutine());
+    }
+
+    IEnumerator GameOverFlickerRoutine()
+    {
+        while (true)
+        {
+            _gameOverText.text = "게임 오버";
+            yield return new WaitForSeconds(0.5f);
+            _gameOverText.text = "";
+            yield return new WaitForSeconds(0.5f);
+        }
+    }
+}
